ecdddc7de271ae8849bef45fc293d3ebe55a74ae
Jake <jacobandrewminor@gmail.com>
2022-01-30 19:44:17 +0000

    Style Error page

--- frontend/src/views/Error.vue
+++ frontend/src/views/Error.vue
@@ -1,13 +1,15 @@
 <template>
-  <div>
-    <p>Uh oh, the page you requested doesn't exist!</p>
+  <div class="d-flex flex-column align-items-center">
+    <error></error>
+    <h3 class="mt-5">Uh oh, the page you requested doesn't exist!</h3>
   </div>
-
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import Error from '@/assets/Error.svg'
 export default Vue.extend({
-  name: 'Error.vue'
+  name: 'Error.vue',
+  components: { Error }
 })
 </script>

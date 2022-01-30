47794314283e308f6df01a84819c352c39a34874
Jake <jacobandrewminor@gmail.com>
2022-01-30 00:19:35 +0000

    Add Statistics Page

---
+++ frontend/src/components/Admin/MonthlySpendWidget.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-card class="w-50">
+    <h3>Monthly Spend</h3>
+    <b-card-body class="h-100">
+      <div class="d-flex align-items-center h-75 mt-3">
+        <b-progress :max="100" show-progress animated class="w-100">
+          <b-progress-bar :value="percentage" :label="`${percentage.toFixed(2)}%`"></b-progress-bar>
+        </b-progress>
+      </div>
+
+      <div class="text-center d-flex mt-3 flex-column justify-content-end">
+        <h5>Total Monthly Spend: £{{ monthlySpend }}</h5>
+        <h5>Total Monthly Threshold: £{{ monthlyCap }}</h5>
+      </div>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+export default Vue.extend({
+  name: 'MonthlySpendWidget',
+  props: {
+    percentage: Number,
+    monthlySpend: Number,
+    monthlyCap: Number
+  }
+})
+</script>
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,13 +3,16 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import { BootstrapVue, BootstrapVueIcons } from 'bootstrap-vue'
+import VueApexCharts from 'vue-apexcharts'
+import VueSocketIOExt from 'vue-socket.io-extended'
 import './styles/style.scss'
 import './validation'
-import VueSocketIOExt from 'vue-socket.io-extended'
+
 import { io } from 'socket.io-client'
 
 const socket = io('http://localhost:3000')
 
+Vue.use(VueApexCharts)
 Vue.use(VueSocketIOExt, socket)
 Vue.use(BootstrapVue)
 Vue.use(BootstrapVueIcons)
